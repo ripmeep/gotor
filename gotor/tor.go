ac21c63638483e11509cd2f01be3ac6d5257d408
ripmeep <ripmeep@protonmail.com>
2021-11-14 03:05:39 +0000

    Changes to be committed: 	new file:   gotor/tor.go
    Changes not staged for commit:
    	deleted:    tor.go

---
+++ gotor/tor.go
@@ -0,0 +1,90 @@
+/*
+	Author:    ripmeep
+	Instagram: @rip.meep
+	GitHub:    https://github.com/ripmeep/
+*/
+
+package tor
+
+import (
+	"net"
+	"errors"
+	"strconv"
+	"encoding/binary"
+)
+
+type TorConnection struct {
+	Host string
+	SocksPort int
+	ControlPort int
+}
+
+func (t TorConnection) Connect(host string, port int) (net.Conn, error) {
+	con, err := net.Dial("tcp", t.Host + ":" + strconv.Itoa(t.SocksPort))
+
+	if err != nil {
+		return nil, err
+	}
+
+	sport := uint16(port) // CONVERT INTEGER TO UINT16 (SHORT) FOR NETWORK USAGE
+	init := "\x05\x01\x00" // SOCKS5, ONE AUTH, NO AUTHENTICATION
+	reply := make([]byte, 64)
+	var tor_ok byte = 0
+
+	con.Write([]byte(init))
+	con.Read(reply)
+
+	if reply[1] != tor_ok {
+		return nil, errors.New("Tor authentication message was not accepted")
+	}
+
+	reply = make([]byte, 64)
+	ptns := make([]byte, 2)
+
+	binary.BigEndian.PutUint16(ptns, sport)
+
+	host_req := "\x05\x01\x00\x03"
+	host_req += string(len(host))
+	host_req += host
+	host_req += string(ptns)
+
+	con.Write([]byte(host_req))
+	con.Read(reply)
+
+	if reply[1] != tor_ok {
+		return nil, errors.New("Tor connection message was not accepted")
+	}
+
+	return con, nil
+}
+
+func (t TorConnection) Refresh(password string) (bool, error) {
+	con, err := net.Dial("tcp", t.Host + ":" + strconv.Itoa(t.ControlPort))
+
+	if err != nil {
+		return false, err
+	}
+
+	reply := make([]byte, 1024)
+	var tor_ok string = "250 OK"
+	auth := []byte("authenticate \"" + password + "\"\r\n")
+
+	con.Write(auth)
+	con.Read(reply)
+
+	if string(reply[:6]) != tor_ok {
+		return false, errors.New("Tor control authentication failed: " + string(reply))
+	}
+
+	reply = make([]byte, 1024)
+	refresh := []byte("signal newnym\r\n")
+
+	con.Write(refresh)
+	con.Read(reply)
+
+	if string(reply[:6]) != tor_ok {
+		return false, errors.New("Tor control authentication failed: " + string(reply))
+	}
+
+	return true, nil
+}
